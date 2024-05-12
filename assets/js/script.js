eb05e5a14b01fee4d73073359853a5459003d915
kayantaryana <kayantaryana@gmail.com>
2024-05-12 20:15:36 +0700

    Revisi

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,19 +18,9 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
-// Toggle class actve untuk shopping cart
-const shoppingCart = document.querySelector(".shopping-cart");
-
-// Ketika shopping cart di klik
-document.querySelector("#shopping-cart-button").onclick = (e) => {
-  shoppingCart.classList.toggle("active");
-  e.preventDefault();
-};
-
 // Klik di luar elemen
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
-const scb = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -39,8 +29,5 @@ document.addEventListener("click", function (e) {
 
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
-  }
-  if (!scb.contains(e.target) && !shoppingCart.contains(e.target)) {
-    shoppingCart.classList.remove("active");
   }
 });
