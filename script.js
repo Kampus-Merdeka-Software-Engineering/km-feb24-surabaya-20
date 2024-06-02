e93b9c7dbde4ed2873d6c714a3ad0b33ab8f5212
kayantaryana <kayantaryana@gmail.com>
2024-06-03 02:51:18 +0700

    menjalankan form contact
    setelah mengisi form lalu submit, maka pesan akan tersimpan di halaman messages pada dashboard.

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// Cross-browser Solution | For browsers that do not support the scroll-behavior property
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
 // Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
@@ -31,3 +59,21 @@ document.addEventListener("click", function (e) {
     searchForm.classList.remove("active");
   }
 });
+
+// mengelola pesan
+document
+  .getElementById("messageForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const message = document.getElementById("message").value;
+
+    const messages = JSON.parse(localStorage.getItem("messages")) || [];
+    messages.push({ name, email, phone, message });
+    localStorage.setItem("messages", JSON.stringify(messages));
+
+    alert("Pesan telah dikirim!");
+    document.getElementById("messageForm").reset();
+  });
